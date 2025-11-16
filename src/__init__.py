c92598a7c0988c9594c52b641308fb2add9e1412
Patthadon Phengpinij <patthadon.1807@gmail.com>
2025-11-16 15:49:22 +0700

    feat: add __init__ for src

---
+++ src/__init__.py
@@ -0,0 +1,35 @@
+"""
+CEDT-2110403-DSDE-Project Source Package
+
+This package contains data processing pipelines and utilities for the
+Data Science and Data Engineering project.
+
+Usage:
+    # Import everything
+    from src import *
+
+    # Import specific modules
+    from src.pipelines import ProvinceTransformer
+    from src.utils import load_province_whitelist
+
+    # Access modules directly
+    import src
+    transformer = src.pipelines.ProvinceTransformer()
+"""
+
+# Import all submodules and their contents
+from . import pipelines, utils
+
+# Import specific classes and functions for direct access
+from .pipelines import ProvinceTransformer
+from .utils import load_province_whitelist
+
+# Define what gets imported with 'from src import *'
+__all__ = [
+    # Modules
+    "pipelines",
+    "utils",
+    # Classes and Functions
+    "ProvinceTransformer",
+    "load_province_whitelist",
+]
