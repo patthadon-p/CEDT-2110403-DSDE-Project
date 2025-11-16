dbf870d1e414cf7b6e8b84c46ca8e92032746e62
Patthadon Phengpinij <patthadon.1807@gmail.com>
2025-11-16 17:44:05 +0700

    refactor: refactor __init__ of src

--- src/__init__.py
+++ src/__init__.py
@@ -20,29 +20,13 @@ Usage:
 # Import all submodules and their contents
 from . import pipelines, utils
 
-# Import specific classes and functions for direct access
-from .pipelines import (
-    DistrictSubdistrictTransformer,
-    ProvinceTransformer,
-    StateToStatusTransformer,
-)
-from .utils import (
-    load_bangkok_official_area_names,
-    load_province_whitelist,
-    load_status_mapping,
-)
+# Import specific items to make them available at package level
+# from .pipelines import *
+# from .utils import *
 
 # Define what gets imported with 'from src import *'
 __all__ = [
     # Modules
     "pipelines",
     "utils",
-    # Classes
-    "ProvinceTransformer",
-    "DistrictSubdistrictTransformer",
-    "StateToStatusTransformer",
-    # Functions
-    "load_province_whitelist",
-    "load_bangkok_official_area_names",
-    "load_status_mapping",
 ]
